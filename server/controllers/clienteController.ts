c344d8705bf8be98f1c13d448687be6af8e04697
Jefferson <jeffersonbraster@gmail.com>
2021-09-25 00:05:46 -0300

    consulta de cliente por id

--- server/controllers/clienteController.ts
+++ server/controllers/clienteController.ts
@@ -24,6 +24,15 @@ const clienteController = {
       return res.status(500).json({ msg: error });
     }
   },
+  getClienteById: async (req: Request, res: Response) => {
+    try {
+      const cliente = await Clientes.findById(req.params.id);
+
+      res.json(cliente);
+    } catch (error: any) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
 };
 
 export default clienteController;
--- server/routes/clienteRoute.ts
+++ server/routes/clienteRoute.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/cliente", clienteController.register);
 
+router.get("/cliente/:id", clienteController.getClienteById);
+
 export default router;
